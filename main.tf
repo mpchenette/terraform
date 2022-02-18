bad7a93d084099aac373444d6875df79af620150
matthewchenette96@gmail.com <matthewchenette96@gmail.com>
2022-02-18 17:50:50 -0600

    removed az var

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-variable "az" {
-  type = map
-}
+# variable "az" {
+#   type = map
+# }
 
 terraform {
   required_providers {
@@ -14,10 +14,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.az[subscriptionId]
-  client_id       = var.az[clientId]
-  client_secret   = var.az[clientSecret]
-  tenant_id       = var.az[tenantId]
+  subscription_id = var.subscriptionId
+  client_id       = var.clientId
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantId
 }
 
 resource "azurerm_resource_group" "rg" {
